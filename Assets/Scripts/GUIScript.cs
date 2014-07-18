4c9367c17a6a56a573d3d073e508d506d47a0840
Haven Szostak <hjszostak@hotmail.com>
2014-07-18 00:14:17 -0400

    setup scene and started creating objects

---
+++ Assets/Scripts/GUIScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+
+	private GameController gameController;
+	private bool[,] values;
+	private Rect gameBackground;
+
+	// Use this for initialization
+	void Start () {
+		gameController = transform.parent.GetComponent<GameController>();
+
+		Vector3 origin = gameController.gameBackground.transform.position;
+		float size = gameController.gameBackground.renderer.bounds.size.x;
+
+		gameBackground = new Rect (origin.x - size / 2, origin.y + size / 2, size, size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		int level = gameController.getLevel ();
+		values = gameController.getValues ();
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public Color backgroundColor, colorOn, colorOff, backgroundSquare;
+	public float gameSize, spacing;
+	public GameObject gameBackground;
+
+	private int level;
+	private bool[,] values;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameBackground = GameObject.Find ("Game Background");
+
+		level = 1;
+
+		values = new bool[1,1];
+		values [0, 0] = false;
+
+		Camera.main.backgroundColor = backgroundColor;
+		gameBackground.renderer.material.color = backgroundSquare;
+		gameBackground.transform.localScale = new Vector3 (gameSize, gameSize, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getLevel (){
+		return level;
+	}
+
+	public bool[,] getValues() {
+		return values;
+	}
+}
