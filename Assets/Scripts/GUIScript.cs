9e4dabf88e5f4fe1ba09a9da905d683276cea375
Haven Szostak <hjszostak@hotmail.com>
2014-07-21 22:46:53 -0400

    Commented and organized code

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -12,12 +12,13 @@ public class GUIScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//pull game data
 		gameController = transform.parent.GetComponent<GameController>();
 		startLevel = gameController.startLevel;
 		size = gameController.gameSize;
 		topLeft = gameController.transform.position + new Vector3 (-size / 2, size / 2, 0);
-		Debug.Log (topLeft);
 		piece = GameObject.Find ("Piece");
+
 		Setup(startLevel);
 	}
 	
@@ -28,18 +29,24 @@ public class GUIScript : MonoBehaviour {
 
 	void Setup (int level) {
 		squares = new GameObject[level, level];
-		deltaSize = gameController.gameSize / level;
+		deltaSize = gameController.gameSize / level;	//width of each square
 		GameObject curObject;
 		Vector3 pos;
 		for (int i = 0; i < level; i++) {
 			for (int j = 0; j < level; j++) {
+				//locate the position of the square
 				pos = topLeft + new Vector3(deltaSize / 2 + j * deltaSize, -deltaSize / 2 - i * deltaSize, 0);
 				pos.z = -2;
+
+				//create the square and change colour
 				curObject = (GameObject)Instantiate(piece, pos , Quaternion.identity);
 				curObject.renderer.material.color = gameController.colorOff;
+
+				//resize and move square to show spacing
 				curObject.transform.localScale += new Vector3(deltaSize - gameController.spacing, deltaSize - gameController.spacing, 0);
 				curObject.transform.position += new Vector3(0, + gameController.spacing / 16, 0);	// adjust to origin
-				squares[j,i] = curObject;
+
+				squares[j,i] = curObject;	//store the square in the 2d array
 			}
 		}
 	}
--- Assets/Scripts/MouseHandler.cs
+++ Assets/Scripts/MouseHandler.cs
@@ -16,10 +16,11 @@ public class MouseHandler : MonoBehaviour {
 
 	void OnMouseDown() {
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		float size = gameController.gameSize;
+		float size = gameController.gameSize;	//width of the game board
+
 		Vector3 topLeft = gameController.transform.position + new Vector3 (-size / 2, size / 2, 0);
 		mousePosition -= topLeft;	//relative position to corner
 		mousePosition *= gameController.getLevel() / size;	//find what square the mouse is in
 		gameController.buttonClick(Mathf.FloorToInt(mousePosition.x), - Mathf.CeilToInt(mousePosition.y));
-		}
+	}
 }
